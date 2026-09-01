cde4f2ca08a6638b60e18fd4f099bf92a6329527
LENG LYHEANG <duckx590@outlook.com>
2026-09-01 09:29:46 +0000

    hello

---
+++ sources/com/unity3d/services/ads/gmascar/handlers/ScarBannerAdHandler.java
@@ -0,0 +1,47 @@
+package com.unity3d.services.ads.gmascar.handlers;
+
+import com.unity3d.scar.adapter.common.IScarBannerAdListenerWrapper;
+import com.unity3d.services.banners.BannerViewCache;
+import com.unity3d.services.banners.bridge.BannerBridge;
+import com.unity3d.services.core.webview.WebViewApp;
+import com.unity3d.services.core.webview.WebViewEventCategory;
+
+/* JADX INFO: loaded from: classes4.dex */
+public class ScarBannerAdHandler implements IScarBannerAdListenerWrapper {
+    private String _operationId;
+
+    public ScarBannerAdHandler(String str) {
+        this._operationId = str;
+    }
+
+    @Override // com.unity3d.scar.adapter.common.IScarAdListenerWrapper
+    public void onAdLoaded() {
+        BannerViewCache.getInstance().addScarContainer(this._operationId);
+        WebViewApp.getCurrentApp().sendEvent(WebViewEventCategory.BANNER, BannerBridge.BannerEvent.SCAR_BANNER_LOADED, this._operationId);
+    }
+
+    @Override // com.unity3d.scar.adapter.common.IScarAdListenerWrapper
+    public void onAdFailedToLoad(int i, String str) {
+        WebViewApp.getCurrentApp().sendEvent(WebViewEventCategory.BANNER, BannerBridge.BannerEvent.SCAR_BANNER_LOAD_FAILED, this._operationId, Integer.valueOf(i), str);
+    }
+
+    @Override // com.unity3d.scar.adapter.common.IScarAdListenerWrapper
+    public void onAdOpened() {
+        WebViewApp.getCurrentApp().sendEvent(WebViewEventCategory.BANNER, BannerBridge.BannerEvent.SCAR_BANNER_OPENED, this._operationId);
+    }
+
+    @Override // com.unity3d.scar.adapter.common.IScarAdListenerWrapper
+    public void onAdClicked() {
+        WebViewApp.getCurrentApp().sendEvent(WebViewEventCategory.BANNER, BannerBridge.BannerEvent.SCAR_BANNER_CLICKED, this._operationId);
+    }
+
+    @Override // com.unity3d.scar.adapter.common.IScarAdListenerWrapper
+    public void onAdClosed() {
+        WebViewApp.getCurrentApp().sendEvent(WebViewEventCategory.BANNER, BannerBridge.BannerEvent.SCAR_BANNER_CLOSED, this._operationId);
+    }
+
+    @Override // com.unity3d.scar.adapter.common.IScarBannerAdListenerWrapper
+    public void onAdImpression() {
+        WebViewApp.getCurrentApp().sendEvent(WebViewEventCategory.BANNER, BannerBridge.BannerEvent.SCAR_BANNER_IMPRESSION, this._operationId);
+    }
+}
